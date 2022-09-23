d177494c04b029d0b7df34d53065ae6c40ca11bc
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-23 23:07:25 +0900

    define: article

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    // fetch our article here
+    const article = await $content("blog", params.slug).fetch();
+  },
+};
+</script>
